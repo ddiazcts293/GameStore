cd28ae473b78f0ffb65ab781f5e7e9fc1f978d0e
MauricioRG <romeromauri94@gmail.com>
2024-04-17 22:08:47 -0700

    Recommendation

--- src/main/java/com/powerrangers/screen/RecommendationScreen.java
+++ src/main/java/com/powerrangers/screen/RecommendationScreen.java
@@ -8,45 +8,37 @@ package com.powerrangers.screen;
 import java.util.Scanner;
 import com.powerrangers.AppContext;           
 import com.powerrangers.util.*;
-import com.powerrangers.db.DbContext;
-import com.powerrangers.db.types.*;
 public class RecommendationScreen implements ScreenBase
 {
 
     @Override
     public void show(AppContext appContext) {
         Scanner scanner = appContext.getScanner();
-        DbContext dbContext = appContext.getDbContext();
         System.out.println("¡Recomendaciones para tu primer compra!");                
         Menu menu = appContext.createMenu();
         String MenuSelection;
 
         System.out.println("Juegos Más vendidos: ");
-        System.out.println("1.");
-        System.out.println("2.");
-        System.out.println("3.");
-        System.out.println("4.");
-        System.out.println("5.");
+        System.out.println("1.God of war - G4");
+        System.out.println("2.Portal 2 - G2");
+        System.out.println("3.Half-life: alyx - G1");
+        System.out.println("4.Half-life 2 - G0");
+        System.out.println("5.Elden ring - G0");
         System.out.println("");
-
         System.out.println("Juegos más populares: ");
-        System.out.println("1.");
-        System.out.println("2.");
-        System.out.println("3.");
-        System.out.println("4.");
-        System.out.println("5.");
-
+        System.out.println("1.God of war - G4");
+        System.out.println("2.Red Dead redemption 2 - G4");
+        System.out.println("3.Elden ring - G4");
+        System.out.println("4.Portal 2 - G2");
+        System.out.println("5.Persona 5 Royal - G0");
+        System.out.println("");
         System.out.println("Juegos más deseados: ");
-        System.out.println("1.");
-        System.out.println("2.");
-        System.out.println("3");
-        System.out.println("4.");
-        System.out.println("5.");
-
-        
-
-
-
+        System.out.println("1.God of war - G4");
+        System.out.println("2.Half-life: Alyx - G1");
+        System.out.println("3.Half-life 2 - G0");
+        System.out.println("4.Elden Ring - G0");
+        System.out.println("5.Baldur´s Ring - G0");
+        System.out.println("");
         
         
 
